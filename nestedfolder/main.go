4c5c0d70aefd3d7717c51d88d0a4aa8c38cca4c3
ito-yo <ito-yo@softbrain.co.jp>
2022-09-17 12:52:54 +0900

    main.go commit

---
+++ nestedfolder/main.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	"os"
+	// "log"
+)
+
+func main() {
+	// file, err := os.Create("a/one")
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	fmt.Println(os.Environ())
+	
+}
